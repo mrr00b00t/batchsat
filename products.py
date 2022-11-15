3354ba49456496d976732a25470c8d99157e4c4c
mrr00b00t <jass2@ic.ufal.br>
2022-11-15 11:22:01 -0300

    add script to download band from the products listed by the products.py. rewrites some pieces of products.py code.

--- products.py
+++ products.py
@@ -5,49 +5,50 @@ import argparse
 import requests
 import pandas as pd
 import shapely.wkt as wkt
+
+from datetime import date
 from functools import reduce
 from itertools import repeat
-from datetime import date
 from sentinelsat import SentinelAPI
 from concurrent.futures import ThreadPoolExecutor
 
-from random import choice
 
-
-def products_from_crops(
+def products_from_polygons(
     account: tuple,
-    crop_id: str, crop_wkt: str, dt_from: date, dt_to: date,
+    polygon_id: str, polygon_wkt: str, dt_from: date, dt_to: date,
     cloudcoverpercentage: tuple = (0, 100),
-    platformname: str = 'Sentinel-2', producttype: str = 'S2MSI2A'
+    platformname: str = 'Sentinel-2', producttype: str = 'S2MSI2A',
+    processinglevel = 'Level-2A'
 ) -> pd.DataFrame | None:
     
     api = SentinelAPI(account[0], account[1], 'https://apihub.copernicus.eu/apihub')
     products: pd.DataFrame | None = None
     
     try:
-        crop_envelope = wkt.loads(crop_wkt).envelope.wkt
+        polygon_envelope = wkt.loads(polygon_wkt).envelope.wkt
         
         products = api.query(
-            crop_envelope,
+            polygon_envelope,
             date=(dt_from.strftime("%Y%m%d"), dt_to.strftime("%Y%m%d")),
             platformname=platformname,
             producttype=producttype,
+            processinglevel=processinglevel,
             cloudcoverpercentage=cloudcoverpercentage
         )
         
         products = api.to_dataframe(products)
         
-        products['crop'] = products['uuid'].apply(lambda _ : {
-            'crop_id': crop_id,
-            'crop_wkt': crop_wkt,
-            'crop_envelope': crop_envelope
+        products['polygon'] = products['uuid'].apply(lambda _ : {
+            'polygon_id': polygon_id,
+            'polygon_wkt': polygon_wkt,
+            'polygon_envelope': polygon_envelope
         })
-        logging.info(f"Retrieved {len(products)} products from crop {crop_id}")
+        logging.info(f"Retrieved {len(products)} products from polygon {polygon_id}")
     except Exception as e:
         logging.error(
             'Error while retrieving products: ' +
             str(e) +
-            f" Crop {crop_id}"
+            f" polygon {polygon_id}"
         )
     
     return products
@@ -59,13 +60,20 @@ def product_online_status(account: tuple, uuid: str) -> str:
 
     try:
         time.sleep(0.125)
-        status = requests.get(url, auth=account).content.decode('utf-8')
+
+        r = requests.get(url, auth=account)
+        
+        if r.status_code != 200:
+            raise Exception(f'Status code {r.status_code}')
+
+        status = r.content.decode('utf-8')
+
         logging.info(f"{uuid} is online? -> {status}")
     except Exception as e:
         logging.error(
             'Error while checking online: ' +
             str(e) +
-            f" Product {uuid}"
+            f" Product {url}"
         )
         status = 'error'
     
@@ -81,14 +89,18 @@ def trigger_online(account: tuple, uuid: str, online: str) -> str:
 
     try:
         time.sleep(0.125)
-        _ = requests.get(url, auth=account, allow_redirects=True)
+        r = requests.get(url, auth=account, allow_redirects=True)
+
+        if r.status_code != 202:
+            raise Exception(f'Status code {r.status_code}')
+
         status = 'true'
         logging.info(f"Product '{uuid}' successfully triggered to online, try to download in a few hours")
     except Exception as e:
         logging.error(
             'Error while triggering offline product: ' +
             str(e) +
-            f" Product {uuid}"
+            f" Product {url}"
         )
         status = 'error'
     
@@ -96,7 +108,7 @@ def trigger_online(account: tuple, uuid: str, online: str) -> str:
 
 def product_metadata(account: tuple, uuid: str, title: str, online: str) -> str:
 
-    if online == 'false':
+    if online != 'true':
         return 'error'
 
     url: str = f"https://apihub.copernicus.eu/apihub/odata/v1/Products('{uuid}')/Nodes('{title}.SAFE')/Nodes('MTD_MSIL2A.xml')/$value"
@@ -104,13 +116,19 @@ def product_metadata(account: tuple, uuid: str, title: str, online: str) -> str:
 
     try:
         time.sleep(0.125)
-        metadata = requests.get(url, auth=account).content.decode('utf-8')
+        r = requests.get(url, auth=account)
+
+        if r.status_code != 200:
+            raise Exception(f'Status code {r.status_code}')
+
+        metadata = r.content.decode('utf-8').replace('\n', '')
+
         logging.info(f"Product {uuid} metadata successfully retrieved")
     except Exception as e:
         logging.error(
             'Error while retrieving metadata: ' +
             str(e) +
-            f" Product {uuid}"
+            f" Product {url}"
         )
         metadata = 'error'
 
@@ -126,9 +144,9 @@ def repeat_list(_list: list, size: int) -> list:
     return new_list[:size]
 
 def main(
-    csv_path: str,
-    random_state: int = 42, samples: int = 30,
-    max_workers: int = 10, cloudcoverpercentage: tuple = (0, 30)
+    csv_path: str, samples: int,
+    random_state: int, max_workers: int,
+    cloudcoverpercentage: tuple
 ) -> None:
 
     logging.basicConfig(level=logging.INFO)
@@ -137,71 +155,81 @@ def main(
         csv_path, sep=';', low_memory = False,
         dtype = str,
         parse_dates=['dt_from', 'dt_to']
-    ).sample(samples, random_state = random_state)
+    )
 
-    accs = [('username', 'password')]
+    if samples is not None:
+        df = df.sample(samples, random_state = random_state)
+
+    accs = [('copernicus_user', 'copernicus_password')]
     accounts = repeat_list(accs, len(df))
 
     concat_dfs = lambda x, y: pd.concat([x, y], ignore_index=True)
-    is_crop_inside = lambda x: wkt.loads(x['footprint']).contains(wkt.loads(x['crop']['crop_envelope']))
+    is_polygon_inside = lambda x: wkt.loads(x['footprint']).contains(wkt.loads(x['polygon']['polygon_envelope']))
     is_not_none = lambda x: x is not None
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
 
-        # get all the products and filter products that not fully contain its crop
-        seq = executor.map(products_from_crops, accounts, df.crop_id, df.crop_wkt, df.dt_from, df.dt_to, repeat(cloudcoverpercentage))
-        df = filter(is_not_none, df)
+        # get all the products and filter products that not fully contain its polygon
+        seq = executor.map(products_from_polygons, accounts, df.polygon_id, df.polygon_wkt, df.dt_from, df.dt_to, repeat(cloudcoverpercentage))
+        seq = filter(is_not_none, seq)
         df = reduce(concat_dfs, seq)
-        df = df[df.apply(is_crop_inside, axis=1)]
+        df = df[df.apply(is_polygon_inside, axis=1)]
         size_before = df.shape[0]
 
-        # group crops by products, they might have repeated crops per product
+        # group polygons by products, they might have repeated polygons per product
         df  = df.groupby(
-            ['uuid', 'title', 'link', 'link_alternative', 'summary', 'footprint', 'filename', 'identifier'],
+            ['uuid', 'title', 'link', 'link_alternative', 'summary', 'footprint', 'filename', 'identifier', 'ondemand'],
             as_index=False
-        ).agg({'crop': lambda x: list(x)})
+        ).agg({'polygon': lambda x: list(x)})
         size_after = df.shape[0]
 
         logging.info(
             f"Processing {size_after} products. " +
-            f"Reusing products that contains one or more crops (Reduction rate: {1. - size_after/size_before})"
+            f"Reusing products that contains one or more polygons (Reduction: {round(100*(1. - size_after/size_before),2)}%)"
         )
 
-        df = df.rename(columns={'crop': 'crops'})
-        df['crops'] = df['crops'].apply(json.dumps)
+        df = df.rename(columns={'polygon': 'polygons'})
+        df['polygons'] = df['polygons'].apply(json.dumps)
 
         # check if all the products is online and ready to download
         accounts = repeat_list(accs, len(df))
-        df['online'] = [r for r in executor.map(product_online_status, accounts, df.uuid)]
+        seq = executor.map(product_online_status, accounts, df.uuid)
+        df['online'] = list(seq)
 
         # trigger the online status from offline products
-        df['triggered'] = [r for r in executor.map(trigger_online, accounts, df.uuid, df.online)]
+        seq = executor.map(trigger_online, accounts, df.uuid, df.online)
+        df['triggered'] = list(seq)
 
         # get metadata from online products
-        df['metadata'] = [r for r in executor.map(product_metadata, accounts, df.uuid, df.title, df.online)]
+        seq = executor.map(product_metadata, accounts, df.uuid, df.title, df.online)
+        df['metadata'] = list(seq)
+
+        df = df.query('online == \'true\' and metadata != \'error\'')
+
+        df.to_csv('products-polygons.csv', sep=';', index=False)
 
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(description='Search and retrieve products based on crop polygons and temporal window.')
+    parser = argparse.ArgumentParser(description='Search and retrieve products based on polygon polygons and temporal window.')
 
     parser.add_argument('--csv-path',
-        help='file path for the csv with crops id, wkt and temporal window',
+        help='file path for the csv with polygons id, wkt and temporal window',
         required=True
     )
 
+    parser.add_argument('--samples',
+        type=int, default=None,
+        help='an integer for the number of samples (default: None, i.e. use all)'
+    )
+
     parser.add_argument('--random-state',
-        type=int,
+        type=int, default=42,
         help='an integer for the random state'
     )
 
-    parser.add_argument('--samples',
-        type=int, default=30,
-        help='an integer for the number of samples (default: 30, i.e. use all)'
-    )
-
     parser.add_argument('--max-workers',
-        type=int, default=10,
-        help='an integer for the number of concurrent threads to execute Copernicus api calls (default: 10)'
+        type=int, default=5,
+        help='an integer for the number of concurrent threads to execute Copernicus api calls (default: 5)'
     )
 
     parser.add_argument('--cloudcoverpercentage',
